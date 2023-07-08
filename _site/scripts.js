9a700797549634890ed17ebbb4d755ef249eebc5
ACE0327 <128328109+Carreno-J@users.noreply.github.com>
2023-07-07 21:40:37 -0700

    PopUp Gallery Alt

--- _site/scripts.js
+++ _site/scripts.js
@@ -20,3 +20,8 @@ lightGallery(document.getElementById('lightgallery'), {
   download: false
   
 });
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false
+  
+});
